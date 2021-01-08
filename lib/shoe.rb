a50e199cddfe1c1b11427400c64d0239a15cbb71
Ryan Pearce <ryan.pearce@key.me>
2021-01-08 19:11:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,21 +1,17 @@
-# Make your shoe class here!
-class Shoe 
+
   
-  attr_accessor :color, :size, :material, :condition 
-  attr_reader :brand
-  
+  class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
   def initialize(brand)
     @brand = brand
   end
-  
+
   def cobble
-    @cobble = "new" 
+    @condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
 end
-  
-  
   
   
   #class Book
