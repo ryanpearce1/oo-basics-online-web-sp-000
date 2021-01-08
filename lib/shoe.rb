409d259df5a5aaa9d84d96068e616f92388e6a40
Ryan Pearce <ryan.pearce@key.me>
2021-01-08 19:11:36 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -15,20 +15,7 @@ class Shoe
   
 end
   
-  
-  class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition
 
-  def initialize(brand)
-    @brand = brand
-  end
-
-  def cobble
-    @condition = "new"
-    puts "Your shoe is as good as new!"
-  end
-end
-  
   
   #class Book
  # attr_accessor :author, :page_count, :genre
