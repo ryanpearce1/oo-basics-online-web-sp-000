57a440ac93e1846e8e21a95b6401564f6d3fe3bb
Ryan Pearce <ryan.pearce@key.me>
2021-01-08 19:10:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,8 @@
 # Make your shoe class here!
 class Shoe 
   
-  attr_accessor :color, :size, :material, :condition 
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition, :brand
+ 
   
   def initialize(brand)
     @brand = brand
