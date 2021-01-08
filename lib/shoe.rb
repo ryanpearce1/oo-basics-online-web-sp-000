4c82a9dce6fbd5f302a66c4dac4db668efca53e1
Ryan Pearce <ryan.pearce@key.me>
2021-01-08 19:02:29 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,27 @@
 # Make your shoe class here!
 class Shoe 
   
+  attr_accessor :color :
+  attr_reader :
+  
+  def initilize (brand)
+    @brand = brand
+  end
+  
+  
+  
+  
+  
+  #class Book
+ # attr_accessor :author, :page_count, :genre
+  #attr_reader :title
+ 
+  #def initialize(title)
+  #  @title = title
+  #end
+ 
+  #def turn_page
+   # puts "Flipping the page...wow, you read fast!"
+  #end
   
   
